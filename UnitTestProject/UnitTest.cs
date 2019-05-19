2445f9e0b0a1c2af6649fd9a968d154adc404932
manaovella <mazz3tt0@gmail.com>
2019-05-19 18:22:35 +0200

    first commit

---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,363 @@
+using mdsplib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Numerics;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class DFTTest
+    {
+        [TestMethod]
+        public void Basic()
+        {
+            // Generate a test signal,
+            //      1 Vrms at 20,000 Hz
+            //      Sampling Rate = 100,000 Hz
+            //      DFT Length is 1000 Points
+            double amplitude = 1.0;
+            double frequency = 20000;
+            UInt32 length = 1000;
+            double samplingRate = 100000;
+
+            double[] inputSignal = DSP.Generate.ToneSampling(amplitude, frequency, samplingRate, length);
+
+            // Instantiate a new DFT
+            DFT dft = new DFT();
+
+            // Initialize the DFT
+            // You only need to do this once or if you change any of the DFT parameters.
+            dft.Initialize(length);
+
+            // Call the DFT and get the scaled spectrum back
+            Complex[] cSpectrum = dft.Execute(inputSignal);
+
+            // Convert the complex spectrum to magnitude
+            double[] lmSpectrum = DSP.ConvertComplex.ToMagnitude(cSpectrum);
+
+            // Note: At this point lmSpectrum is a 501 byte array that 
+            // contains a properly scaled Spectrum from 0 - 50,000 Hz
+
+            // For plotting on an XY Scatter plot generate the X Axis frequency Span
+            double[] freqSpan = dft.FrequencySpan(samplingRate);
+
+            // At this point a XY Scatter plot can be generated from,
+            // X axis => freqSpan
+            // Y axis => lmSpectrum
+
+            // In this example the maximum value of 1 Vrms is located at bin 200 (20,000 Hz)
+        }
+
+        [TestMethod]
+        public void BasicWnd()
+        {
+            // Same Input Signal as Example 1
+            double amplitude = 1.0; double frequency = 20000;
+            UInt32 length = 1000;
+            double samplingRate = 100000;
+            double[] inputSignal = DSP.Generate.ToneSampling(amplitude, frequency, samplingRate, length);
+
+            // Apply window to the Input Data & calculate Scale Factor
+            double[] wCoefs = DSP.Window.Coefficients(DSP.Window.Type.Hamming, length);
+            double[] wInputData = DSP.Math.Multiply(inputSignal, wCoefs);
+            double wScaleFactor = DSP.Window.ScaleFactor.Signal(wCoefs);
+
+            // Instantiate & Initialize a new DFT
+            DFT dft = new DFT();
+            dft.Initialize(length);
+
+            // Call the DFT and get the scaled spectrum back
+            Complex[] cSpectrum = dft.Execute(wInputData);
+
+            // Convert the complex spectrum to note: Magnitude Format
+            double[] lmSpectrum = DSP.ConvertComplex.ToMagnitude(cSpectrum);
+
+            // Properly scale the spectrum for the added window
+            lmSpectrum = DSP.Math.Multiply(lmSpectrum, wScaleFactor);
+
+            // For plotting on an XY Scatter plot generate the X Axis frequency Span
+            double[] freqSpan = dft.FrequencySpan(samplingRate);
+
+            // At this point a XY Scatter plot can be generated from,
+            // X axis => freqSpan
+            // Y axis => lmSpectrum
+        }
+
+        [TestMethod]
+        public void BasicWndZeroPad()
+        {
+            // Same Input Signal as Example 1, except 5 Vrms amplitude
+            double amplitude = 5.0; double frequency = 20000;
+            UInt32 length = 1000; double samplingRate = 100000;
+            double[] inputSignal = DSP.Generate.ToneSampling(amplitude, frequency, samplingRate, length);
+
+            // Apply window to the Input Data & calculate Scale Factor
+            double[] wCoefs = DSP.Window.Coefficients(DSP.Window.Type.FTHP, length);
+            double[] wInputData = DSP.Math.Multiply(inputSignal, wCoefs);
+            double wScaleFactor = DSP.Window.ScaleFactor.Signal(wCoefs);
+
+            // Instantiate & Initialize a new DFT w/Zero Padding (5000 points)
+            DFT dft = new DFT();
+            dft.Initialize(length, 5000);
+
+            // Call the DFT and get the scaled spectrum back
+            Complex[] cSpectrum = dft.Execute(wInputData);
+
+            // Convert the complex spectrum to note: Magnitude Format
+            double[] lmSpectrum = DSP.ConvertComplex.ToMagnitude(cSpectrum);
+
+            // Properly scale the spectrum for the added window
+            lmSpectrum = DSP.Math.Multiply(lmSpectrum, wScaleFactor);
+
+            // For plotting on an XY Scatter plot generate the X Axis frequency Span
+            double[] freqSpan = dft.FrequencySpan(samplingRate);
+
+            // At this point a XY Scatter plot can be generated from,
+            // X axis => freqSpan
+            // Y axis => lmSpectrum
+
+            // For this example - the maximum value of 5 Vrms is at bin 1200 (20,000 Hz)
+        }
+
+        [TestMethod]
+        public void BasicWndLogMag()
+        {
+            // Same Input Signal as Example 1, except amplitude is 5 Vrms.
+            double amplitude = 5.0; double frequency = 20000;
+            UInt32 length = 1000; double samplingRate = 100000;
+            double[] inputSignal = DSP.Generate.ToneSampling(amplitude, frequency, samplingRate, length);
+
+            // Apply window to the Input Data & calculate Scale Factor
+            double[] wCoefs = DSP.Window.Coefficients(DSP.Window.Type.FTHP, length);
+            double[] wInputData = DSP.Math.Multiply(inputSignal, wCoefs);
+            double wScaleFactor = DSP.Window.ScaleFactor.Signal(wCoefs);
+
+            // Instantiate & Initialize a new DFT
+            DFT dft = new DFT();
+            dft.Initialize(length);
+
+            // Call the DFT and get the scaled spectrum back
+            Complex[] cSpectrum = dft.Execute(wInputData);
+
+            // Convert the complex spectrum to note: Magnitude Format
+            double[] lmSpectrum = DSP.ConvertComplex.ToMagnitude(cSpectrum);
+
+            // Properly scale the spectrum for the added window
+            lmSpectrum = DSP.Math.Multiply(lmSpectrum, wScaleFactor);
+
+            // Convert from linear magnitude to log magnitude format
+            double[] logMagSpectrum = DSP.ConvertMagnitude.ToMagnitudeDBV(lmSpectrum);
+
+            // For plotting on an XY Scatter plot generate the X Axis frequency Span
+            double[] freqSpan = dft.FrequencySpan(samplingRate);
+
+            // At this point a XY Scatter plot can be generated from,
+            // X axis => freqSpan
+            // Y axis => logMagSpectrum
+
+            // In this example - maximum amplitude of 13.974 dBV is at bin 200 (20,000 Hz)
+        }
+
+        [TestMethod]
+        public void NoiseAnalysis()
+        {
+            // Setup parameters to generate noise test signal of 5 nVrms / rt-Hz
+            double amplitude = 5.0e-9;
+            UInt32 length = 1000; double samplingRate = 2000;
+
+
+            // Generate window & calculate Scale Factor for NOISE!
+            double[] wCoefs = DSP.Window.Coefficients(DSP.Window.Type.Hamming, length);
+
+            double wScaleFactor = DSP.Window.ScaleFactor.Noise(wCoefs, samplingRate);
+
+            // Instantiate & Initialize a new DFT
+            DFT dft = new DFT();
+            dft.Initialize(length);
+
+            // Average the noise 'N' times
+            Int32 N = 1000;
+            double[] noiseSum = new double[(length / 2) + 1];
+            for (Int32 i = 0; i < N; i++)
+            {
+                // Generate the noise signal & apply window
+                double[] inputSignal = DSP.Generate.NoisePsd(amplitude, samplingRate, length);
+                inputSignal = DSP.Math.Multiply(inputSignal, wCoefs);
+
+                // DFT the noise -> Convert -> Sum
+                Complex[] cSpectrum = dft.Execute(inputSignal);
+                double[] mag2 = DSP.ConvertComplex.ToMagnitudeSquared(cSpectrum);
+
+                if (N == 0)
+                    noiseSum = DSP.Math.Add(noiseSum, 0);
+                else
+                    noiseSum = DSP.Math.Add(noiseSum, mag2);
+            }
+
+            // Calculate Average, convert to magnitude format
+            // See text for the reasons to use Mag^2 format.
+            double[] averageNoise = DSP.Math.Divide(noiseSum, N);
+            double[] lmSpectrum = DSP.ConvertMagnitudeSquared.ToMagnitude(averageNoise);
+
+            // Properly scale the spectrum for the added window
+            lmSpectrum = DSP.Math.Multiply(lmSpectrum, wScaleFactor);
+
+            // For plotting on an XY Scatter plot generate the X Axis frequency Span
+            double[] freqSpan = dft.FrequencySpan(samplingRate);
+
+            // At this point a XY Scatter plot can be generated from,
+            // X axis => freqSpan
+            // Y axis => lmSpectrum as a Power Spectral Density Value
+
+            // Extra Credit - Analyze Plot Data Ignoring the first and last 20 Bins
+            // Average value should be what we generated = 5.0e-9 Vrms / rt-Hz
+            double averageValue = DSP.Analyze.FindMean(lmSpectrum, 20, 20);
+        }
+
+        [TestMethod]
+        public void PhaseAnalysis()
+        {
+            // Generate a Phase Ramp between two signals
+            double[] resultPhase = new double[360];
+
+            // Instantiate & Initialize a new DFT
+            DFT dft = new DFT();
+            dft.Initialize(2048);
+
+            for (Int32 phase = 0; phase < 360; phase++)
+            {
+                double[] inputSignalRef = DSP.Generate.ToneCycles(7.0, 128, 2048);
+                double[] inputSignalPhase = DSP.Generate.ToneCycles(7.0, 128, 2048, phaseDeg: phase);
+
+                // Call the DFT and get the scaled spectrum back of a reference and a phase shifted signal.
+                Complex[] cSpectrumRef = dft.Execute(inputSignalRef);
+                Complex[] cSpectrumPhase = dft.Execute(inputSignalPhase);
+
+                // Magnitude Format - Just as a test point
+                //double[] lmSpectrumTest = DSPLib.DSP.ConvertComplex.ToMagnitude(cSpectrumRef);
+                //double[] lmSpectrumTestPhase = DSPLib.DSP.ConvertComplex.ToMagnitude(cSpectrumPhase);
+
+                // Extract the phase of bin 128
+                double[] resultArrayRef = DSP.ConvertComplex.ToPhaseDegrees(cSpectrumRef);
+                double[] resultArrayPhase = DSP.ConvertComplex.ToPhaseDegrees(cSpectrumPhase);
+                resultPhase[phase] = resultArrayPhase[128] - resultArrayRef[128];
+            }
+
+            // resultPhase has a linear phase incrementing signal at this point.
+            // Use UnwrapPhase() to remove phase jumps in output data.
+        }
+    }
+
+    [TestClass]
+    public class FFTTest
+    {
+        [TestMethod]
+        public void Basic()
+        {
+            // Same Input Signal as Example 1, except everything is a power of two
+            double amplitude = 1.0; double frequency = 32768;
+            UInt32 length = 1024; double samplingRate = 131072;
+            double[] inputSignal = DSP.Generate.ToneSampling(amplitude, frequency, samplingRate, length);
+
+            // Apply window to the Input Data & calculate Scale Factor
+            double[] wCoefs = DSP.Window.Coefficients(DSP.Window.Type.Hamming, length);
+            double[] wInputData = DSP.Math.Multiply(inputSignal, wCoefs);
+            double wScaleFactor = DSP.Window.ScaleFactor.Signal(wCoefs);
+
+            // Instantiate & Initialize a new DFT
+            FFT fft = new FFT();
+            fft.Initialize(length);
+
+            // Call the FFT and get the scaled spectrum back
+            Complex[] cSpectrum = fft.Execute(wInputData);
+
+            // Convert the complex spectrum to note: Magnitude Squared Format
+            // See text for the reasons to use Mag^2 format.
+            double[] lmSpectrum = DSP.ConvertComplex.ToMagnitude(cSpectrum);
+
+            // Properly scale the spectrum for the added window
+            lmSpectrum = DSP.Math.Multiply(lmSpectrum, wScaleFactor);
+
+            // For plotting on an XY Scatter plot generate the X Axis frequency Span
+            double[] freqSpan = fft.FrequencySpan(samplingRate);
+
+            // At this point a XY Scatter plot can be generated from,
+            // X axis => freqSpan
+            // Y axis => lmSpectrum
+        }
+
+        [TestMethod]
+        public void BasicWndZeroPad()
+        {
+            // Same Input Signal as Example 1, except everything is a power of two
+            double amplitude = 1.0; double frequency = 32768;
+            UInt32 length = 1024; double samplingRate = 131072;
+            double[] inputSignal = DSP.Generate.ToneSampling(amplitude, frequency, samplingRate, length);
+
+            // Apply window to the Input Data & calculate Scale Factor
+            double[] wCoefs = DSP.Window.Coefficients(DSP.Window.Type.Hamming, length);
+            double[] wInputData = DSP.Math.Multiply(inputSignal, wCoefs);
+            double wScaleFactor = DSP.Window.ScaleFactor.Signal(wCoefs);
+
+            // Instantiate & Initialize a new DFT
+            FFT fft = new FFT();
+            fft.Initialize(length, length * 3);           // Zero Padding = 1024 * 3
+
+            // Call the FFT and get the scaled spectrum back
+            Complex[] cSpectrum = fft.Execute(wInputData);
+
+            // Convert the complex spectrum to note: Magnitude Squared Format
+            // See text for the reasons to use Mag^2 format.
+            double[] lmSpectrum = DSP.ConvertComplex.ToMagnitude(cSpectrum);
+
+            // Properly scale the spectrum for the added window
+            lmSpectrum = DSP.Math.Multiply(lmSpectrum, wScaleFactor);
+
+            // For plotting on an XY Scatter plot generate the X Axis frequency Span
+            double[] freqSpan = fft.FrequencySpan(samplingRate);
+
+            // At this point a XY Scatter plot can be generated from,
+            // X axis => freqSpan
+            // Y axis => lmSpectrum
+        }
+
+        [TestMethod]
+        public void PhaseUnwrap()
+        {
+            // Generate a Phase Ramp between two signals
+            double[] resultPhase = new double[600];
+            double[] unwrapPhase = new double[600];
+
+            UInt32 length = 2048;
+            double[] wCoeff = DSP.Window.Coefficients(DSP.Window.Type.FTHP, length);
+
+            // Instantiate & Initialize a new DFT
+            FFT fft = new FFT();
+            fft.Initialize(length, 3 * length);
+
+            for (Int32 phase = 0; phase < 600; phase++)
+            {
+                double[] inputSignalRef = DSP.Generate.ToneCycles(7.0, 128, length, phaseDeg: 45.0);
+                double[] inputSignalPhase = DSP.Generate.ToneCycles(7.0, 128, length, phaseDeg: phase);
+
+                inputSignalRef = DSP.Math.Multiply(inputSignalRef, wCoeff);
+                inputSignalPhase = DSP.Math.Multiply(inputSignalPhase, wCoeff);
+
+                // Call the DFT and get the scaled spectrum back of a reference and a phase shifted signal.
+                Complex[] cSpectrumRef = fft.Execute(inputSignalRef);
+                Complex[] cSpectrumPhase = fft.Execute(inputSignalPhase);
+
+                // Magnitude Format - Just as a test point
+                double[] lmSpectrumTest = DSP.ConvertComplex.ToMagnitude(cSpectrumRef);
+                UInt32 peakLocation = DSP.Analyze.FindMaxPosition(lmSpectrumTest);
+
+                // Extract the phase of 'peak value' bin
+                double[] resultArrayRef = DSP.ConvertComplex.ToPhaseDegrees(cSpectrumRef);
+                double[] resultArrayPhase = DSP.ConvertComplex.ToPhaseDegrees(cSpectrumPhase);
+                resultPhase[phase] = resultArrayPhase[peakLocation] - resultArrayRef[peakLocation];
+
+            }
+            unwrapPhase = DSP.Analyze.UnwrapPhaseDegrees(resultPhase);
+        }
+    }
+}
---
+++ mdsplib/dft.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace mdsplib
+{
+
+    #region =====[ DFT Core Class ]======================================================
+
+    /// <summary>
+    /// DFT Base Class
+    /// </summary>
+    public class DFT
+    {
+        /// <summary>
+        /// DFT Class
+        /// </summary>
+        public DFT() { }
+
+        #region Properties
+
+        private double mDFTScale;       // DFT ONLY Scale Factor
+        private UInt32 mLengthTotal;    // mN + mZp
+        private UInt32 mLengthHalf;     // (mN + mZp) / 2
+
+        private double[,] mCosTerm;      // Caching of multiplication terms to save time
+        private double[,] mSinTerm;      // on smaller DFT's
+        private bool mOutOfMemory;       // True = Caching ran out of memory.
+
+
+        /// <summary>
+        /// Read only Boolean property. True meas the currently defined DFT is using cached memory to speed up calculations.
+        /// </summary>
+        public bool IsUsingCached
+        {
+            private set { }
+            get { return !mOutOfMemory; }
+        }
+
+        #endregion
+
+        #region Core DFT Routines
+
+        /// <summary>
+        /// Pre-Initializes the DFT.
+        /// Must call first and this anytime the FFT setup changes.
+        /// </summary>
+        /// <param name="inputDataLength"></param>
+        /// <param name="zeroPaddingLength"></param>
+        /// <param name="forceNoCache">True will force the DFT to not use pre-calculated caching.</param>
+        public void Initialize(UInt32 inputDataLength, UInt32 zeroPaddingLength = 0, bool forceNoCache = false)
+        {
+            // Save the sizes for later
+            mLengthTotal = inputDataLength + zeroPaddingLength;
+            mLengthHalf = (mLengthTotal / 2) + 1;
+
+            // Set the overall scale factor for all the terms
+            mDFTScale = Math.Sqrt(2) / (double)(inputDataLength + zeroPaddingLength);                 // Natural DFT Scale Factor                                           // Window Scale Factor
+            mDFTScale *= ((double)(inputDataLength + zeroPaddingLength)) / (double)inputDataLength;   // Account For Zero Padding                           // Zero Padding Scale Factor
+
+
+            if (forceNoCache == true)
+            {
+                // If optional No Cache - just flag that the cache failed 
+                // then the routines will use the brute force DFT methods.
+                mOutOfMemory = true;
+                return;
+            }
+
+            // Try to make pre-calculated sin/cos arrays. If not enough memory, then 
+            // use a brute force DFT.
+            // Note: pre-calculation speeds the DFT up by about 5X (on a core i7)
+            mOutOfMemory = false;
+            try
+            {
+                mCosTerm = new double[mLengthTotal, mLengthTotal];
+                mSinTerm = new double[mLengthTotal, mLengthTotal];
+
+                double scaleFactor = 2.0 * Math.PI / mLengthTotal;
+
+                //Parallel.For(0, mLengthHalf, (j) =>
+                for (int j = 0; j < mLengthHalf; j++)
+                {
+                    double a = j * scaleFactor;
+                    for (int k = 0; k < mLengthTotal; k++)
+                    {
+                        mCosTerm[j, k] = Math.Cos(a * k) * mDFTScale;
+                        mSinTerm[j, k] = Math.Sin(a * k) * mDFTScale;
+                    }
+                } //);
+            }
+            catch (OutOfMemoryException)
+            {
+                // Could not allocate enough room for the cache terms
+                // So, will use brute force DFT
+                mOutOfMemory = true;
+            }
+        }
+
+
+        /// <summary>
+        /// Execute the DFT.
+        /// </summary>
+        /// <param name="timeSeries"></param>
+        /// <returns>Complex[] FFT Result</returns>
+        public Complex[] Execute(double[] timeSeries)
+        {
+            Debug.Assert(timeSeries.Length <= mLengthTotal, "The input timeSeries length was greater than the total number of points that was initialized. DFT.Exectue()");
+
+            // Account for zero padding in size of DFT input array
+            double[] totalInputData = new double[mLengthTotal];
+            Array.Copy(timeSeries, totalInputData, timeSeries.Length);
+
+            Complex[] output;
+            if (mOutOfMemory)
+                output = Dft(totalInputData);
+            else
+                output = DftCached(totalInputData);
+
+            return output;
+        }
+
+        #region Private DFT Implementation details
+
+        /// <summary>
+        /// A brute force DFT - Uses Task / Parallel pattern
+        /// </summary>
+        /// <param name="timeSeries"></param>
+        /// <returns>Complex[] result</returns>
+        private Complex[] Dft(double[] timeSeries)
+        {
+            UInt32 n = mLengthTotal;
+            UInt32 m = mLengthHalf;
+            double[] re = new double[m];
+            double[] im = new double[m];
+            Complex[] result = new Complex[m];
+            double sf = 2.0 * Math.PI / n;
+
+            Parallel.For(0, m, (j) =>
+            //for (UInt32 j = 0; j < m; j++)
+            {
+                double a = j * sf;
+                for (UInt32 k = 0; k < n; k++)
+                {
+                    re[j] += timeSeries[k] * Math.Cos(a * k) * mDFTScale;
+                    im[j] -= timeSeries[k] * Math.Sin(a * k) * mDFTScale;
+                }
+
+                result[j] = new Complex(re[j], im[j]);
+            });
+
+            // DC and Fs/2 Points are scaled differently, since they have only a real part
+            result[0] = new Complex(result[0].Real / Math.Sqrt(2), 0.0);
+            result[mLengthHalf - 1] = new Complex(result[mLengthHalf - 1].Real / Math.Sqrt(2), 0.0);
+
+            return result;
+        }
+
+        /// <summary>
+        /// DFT with Pre-calculated Sin/Cos arrays + Task / Parallel pattern.
+        /// DFT can only be so big before the computer runs out of memory and has to use
+        /// the brute force DFT.
+        /// </summary>
+        /// <param name="timeSeries"></param>
+        /// <returns>Complex[] result</returns>
+        private Complex[] DftCached(double[] timeSeries)
+        {
+            UInt32 n = mLengthTotal;
+            UInt32 m = mLengthHalf;
+            double[] re = new double[m];
+            double[] im = new double[m];
+            Complex[] result = new Complex[m];
+
+            Parallel.For(0, m, (j) =>
+            //for (UInt32 j = 0; j < m; j++)
+            {
+                for (UInt32 k = 0; k < n; k++)
+                {
+                    re[j] += timeSeries[k] * mCosTerm[j, k];
+                    im[j] -= timeSeries[k] * mSinTerm[j, k];
+                }
+                result[j] = new Complex(re[j], im[j]);
+            });
+
+            // DC and Fs/2 Points are scaled differently, since they have only a real part
+            result[0] = new Complex(result[0].Real / Math.Sqrt(2), 0.0);
+            result[mLengthHalf - 1] = new Complex(result[mLengthHalf - 1].Real / Math.Sqrt(2), 0.0);
+
+            return result;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Utility Functions
+
+        /// <summary>
+        /// Return the Frequency Array for the currently defined DFT.
+        /// Takes into account the total number of points and zero padding points that were defined.
+        /// </summary>
+        /// <param name="samplingFrequencyHz"></param>
+        /// <returns></returns>
+        public double[] FrequencySpan(double samplingFrequencyHz)
+        {
+            UInt32 points = mLengthHalf;
+            double[] result = new double[points];
+            double stopValue = samplingFrequencyHz / 2.0;
+            double increment = stopValue / ((double)points - 1.0);
+
+            for (UInt32 i = 0; i < points; i++)
+                result[i] += increment * i;
+
+            return result;
+        }
+
+    }
+
+    #endregion
+
+    #endregion
+
+}
